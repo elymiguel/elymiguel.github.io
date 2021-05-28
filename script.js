1a2fd1865e97291d4ee208fe4e412317a4d7c09d
elymiguel <user@MacBook-Pro.local>
2021-05-28 09:31:06 +0100

    Firt commit - Uploading Profile

---
+++ script.js
@@ -0,0 +1,11 @@
+// CHANGE BODY TEMA
+
+function changeBodyTheme(){
+    let obj=document.getElementById('body');
+    if(obj.className=='bodyDark'){
+        obj.className='bodyLight';
+    }else{
+        obj.className='bodyDark';
+     }
+    }
+
