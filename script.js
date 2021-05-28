914e5d877dd5e27f1dc6ba99ce2c1473f83eeb04
elymiguel <user@MacBook-Pro.local>
2021-05-28 22:58:03 +0100

    Modifiquei Comentarios

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// FUNCTION - CHANGE BODY TEMA
+// Function - Change Body Theme
 
 function changeBodyTheme(){
     let obj=document.getElementById('body');
@@ -9,7 +9,7 @@ function changeBodyTheme(){
      }
     }
 
-// FUNCTION - SHOW MAIL
+// Function - Show Email
 function myMail(){
     alert('Get in touch: ely.miguel@gmail.com');
     } 
