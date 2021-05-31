d3a880ed3fd9e4c8a244da2bcecb9b57f96aec77
elymiguel <ely.miguel@gmail.com>
2021-05-31 20:00:40 +0100

    Add Side Menu | Add Function Open Menu/Hidden Footer | Add animation sub title

--- script.js
+++ script.js
@@ -13,3 +13,13 @@ function changeBodyTheme(){
 function myMail(){
     alert('Hey! I am glad you are interested in contacting me. Send me a message on my Linkedin profile at any time :)');
     } 
+
+// Function - Nav Bar | Open - Close
+
+    function openNav() {
+        document.getElementById("mySidenav").style.width = "250px";
+      }
+      
+      function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+      }
