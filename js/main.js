f69bd7a76c0f43b737859bcff8bdff6ac2d4452a
elymiguel <ely.miguel@gmail.com>
2021-06-02 12:44:23 +0100

    Script in html to folde JS ->  main.js

---
+++ js/main.js
@@ -0,0 +1,38 @@
+// PROFILE PAGE 
+// Function - Change Body Theme
+
+function changeBodyTheme(){
+    let obj=document.getElementById('body');
+    if(obj.className=='bodyDark'){
+        obj.className='bodyLight';
+    }else{
+        obj.className='bodyDark';
+     }
+    }
+
+
+// PROJECT PAGE 
+// Function - Nav Bar | Open - Close 
+
+setTimeout(function() { 
+    document.querySelector('.title').classList.add('animate');
+    document.querySelector('.sub-title').classList.add('animate');
+    
+    
+},1)
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("myContent").style.opacity = 0.85;
+    document.getElementById("footer").style.display = "none";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.getElementById("myContent").style.opacity = 1;
+    document.getElementById("footer").style.display = "flex";
+}
+
+
